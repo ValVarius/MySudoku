e4bd6131d13033f86770204613555d1c07b91406
ValVarius <60993964+ValVarius@users.noreply.github.com>
2023-01-30 10:44:21 -0700

    fixing grid to disply game

--- src/components/SudokuGrid/index.js
+++ src/components/SudokuGrid/index.js
@@ -1,22 +1,43 @@
-import React, { useState } from 'react';
-import "./style.css"
+import React, { useState, useEffect  } from "react";
+import "./style.css";
 
-function SudokuGrid({ puzzle }) {
+export default function SudokuGrid({ puzzle }) {
   const [grid, setGrid] = useState(puzzle);
 
+  useEffect(() => {
+    setGrid(puzzle)
+  }, [puzzle]);
+
+  const handleCellChange = (e, rowIndex, columnIndex) => {
+    const newValue = e.target.value;
+    setGrid((prevGrid) =>
+      prevGrid.map((row, i) =>
+        i === rowIndex
+          ? row.map((cell, j) => (j === columnIndex ? newValue : cell))
+          : row
+      )
+    );
+  };
+
   return (
-    <div className="sudoku-grid">
-      {grid.map((row, i) => (
-        <div key={i} className="row">
-          {row.map((cell, j) => (
-            <div key={j} className={`cell ${(i < 3 || i > 5) && (j < 3 || j > 5) ? "bold" : ""}`}>
-              {cell}
-            </div>
-          ))}
+    <div className="grid-container">
+    {grid.map((row, rowIndex) =>
+      row.map((value, columnIndex) => (
+        <div key={`${rowIndex}-${columnIndex}`} className="grid-cell">
+          {value === null ? (
+            <input className = "cellinput" type="text" onChange={(e) => handleCellChange(e, rowIndex, columnIndex)} />
+          ) : (
+            value
+          )}
         </div>
-      ))}
-    </div>
+      ))
+    )}
+  </div>
   );
 }
 
-export default SudokuGrid;
+
+
+
+
+
